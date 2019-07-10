bc84529583c149276017203cefbcd852f5fed932
DexMerc <44553376+DexMerc@users.noreply.github.com>
2019-07-10 14:00:21 +0600

    Add files via upload

---
+++ duplicates_sorted_LL.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None:
+            return head
+
+        curr = head
+        while curr.next is not None:
+            if curr.val == curr.next.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return head
